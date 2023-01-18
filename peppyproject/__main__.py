cec5bbc20b7401835548799e16b55984f9d6f43c
Zach Burnett <zburnett@stsci.edu>
2023-01-18 09:44:53 -0500

    add coverage options and cli

--- peppyproject/__main__.py
+++ peppyproject/__main__.py
@@ -1,5 +1,25 @@
+from pathlib import Path
+
+import typer
+
+from peppyproject import PyProjectConfiguration
+
+
+def peppyproject_command(directory: Path = None, output: Path = None):
+    if directory is None:
+        directory = Path.cwd()
+
+    configuration = PyProjectConfiguration.from_directory(directory=directory)
+    toml_string = configuration.to_toml()
+    if output is None:
+        print(toml_string)
+    else:
+        with open(output, 'w') as toml_file:
+            toml_file.write(toml_string)
+
+
 def main():
-    pass
+    typer.run(peppyproject_command)
 
 
 if __name__ == "__main__":
